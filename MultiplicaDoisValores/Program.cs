7f3b6168537a6c4f3d34cd581a5577be45806560
Gustavo Figueiredo Lino <guguinhajc@gmail.com>
2020-03-03 21:53:46 -0300

    Exercicios do Dia 03-03

---
+++ MultiplicaDoisValores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicaDoisValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, multp;
+
+            Console.Write("Digite o primeiro número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            multp = num1 * num2;
+
+            Console.WriteLine("O Resultado é: " + multp);
+        }
+    }
+}
