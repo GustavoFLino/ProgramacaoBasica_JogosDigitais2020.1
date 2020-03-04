fa519a8f256ada17d2db160f4dc1944d3270a127
Gustavo Figueiredo Lino <guguinhajc@gmail.com>
2020-03-03 21:50:25 -0300

    Exercicios do Dia 03-03
    Exercicios do dia 03-03

---
+++ ConversaoDeTipos/conversão.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConversaoDeTipos
+{
+    class conversão
+    {
+        static void Main(string[] args)
+        {
+            string a = "11", b = "2";
+
+            int c = Convert.ToInt32(a) / Convert.ToInt32(b);
+
+            Console.WriteLine(c);
+
+            String d = Convert.ToString(c);
+
+            Console.WriteLine(c.GetType());
+
+            Console.WriteLine(d);
+
+            Console.WriteLine(d.GetType());
+
+     
+        }
+    }
+}
---
+++ EntradaESaida/EntradaESaida.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EntradaESaida
+{
+    class entradaESaida
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2,soma;
+
+            Console.Write("Digite o primeiro número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o segundo número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = num1 + num2;
+
+            Console.WriteLine("O Resultado é: " + soma);
+
+        }
+    }
+}
